f73f6c76f2199f24771b4821d58d601c7679f393
manas gupta <manasgupta0110@gmail.com>
2023-05-04 22:26:21 +0530

    new

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{() => convertToCelcius(data)}°C</h1> : null}
+            {data.main ? <h1>{convertToCelcius(data)}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
